f8a5065d2f737073619d055f9cf5a0cb2a2d36f3
Landic <danil.volkov.235@gmail.com>
2022-12-25 23:25:15 +0200

    Добавил дз за 21.12 ()

--- HttpExample/HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
+++ HttpExample/HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
@@ -1,45 +1,61 @@
 ﻿#pragma comment (lib, "Ws2_32.lib")
 #include <Winsock2.h>
 #include <ws2tcpip.h>
-#include <string>
 #include <iostream>
-#include <windows.h>
+#include <string>
 #include <fstream>
-#include "ctime"
 using namespace std;
 
+int main()
+{
+    setlocale(0, "ru");
 
-int main() {
+    //1. инициализация "Ws2_32.dll" для текущего процесса
     WSADATA wsaData;
     WORD wVersionRequested = MAKEWORD(2, 2);
+
     int err = WSAStartup(wVersionRequested, &wsaData);
     if (err != 0) {
-        cout << "WSAStartup error -> " << err << endl;
+
+        cout << "WSAStartup failed with error: " << err << endl;
         return 1;
     }
-    char hostname[255] = "api.privatbank.ua";
+
+    //инициализация структуры, для указания ip адреса и порта сервера с которым мы хотим соединиться
+
+    char hostname[255] = "api.openweathermap.org";
+
     addrinfo* result = NULL;
+
     addrinfo hints;
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
+
     int iResult = getaddrinfo(hostname, "http", &hints, &result);
     if (iResult != 0) {
-        cout << "getaddrinfo error -> " << iResult << endl;
+        cout << "getaddrinfo failed with error: " << iResult << endl;
         WSACleanup();
         return 3;
     }
+
     SOCKET connectSocket = INVALID_SOCKET;
     addrinfo* ptr = NULL;
-    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) { 
+
+    //Пробуем присоединиться к полученному адресу
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+        //2. создание клиентского сокета
         connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
         if (connectSocket == INVALID_SOCKET) {
-            printf("Error socket: %ld\n", WSAGetLastError());
+            printf("socket failed with error: %ld\n", WSAGetLastError());
             WSACleanup();
             return 1;
         }
-        iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen); 
+
+        //3. Соединяемся с сервером
+        iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
         if (iResult == SOCKET_ERROR) {
             closesocket(connectSocket);
             connectSocket = INVALID_SOCKET;
@@ -47,45 +63,63 @@ int main() {
         }
         break;
     }
-    string uri = "/p24api/pubinfo?json&exchange&coursid=5";
+
+    //4. HTTP Request
+
+    string uri = "/data/2.5/weather?q=Odessa&appid=75f6e64d49db78658d09cb5ab201e483&mode=JSON";
+
     string request = "GET " + uri + " HTTP/1.1\n";
     request += "Host: " + string(hostname) + "\n";
     request += "Accept: */*\n";
     request += "Accept-Encoding: gzip, deflate, br\n";
     request += "Connection: close\n";
     request += "\n";
-    if (send(connectSocket, request.c_str(), request.length(), 0) == SOCKET_ERROR) { 
-        cout << "Error sending: " << WSAGetLastError() << endl;
+
+    //отправка сообщения
+    if (send(connectSocket, request.c_str(), request.length(), 0) == SOCKET_ERROR) {
+        cout << "send failed: " << WSAGetLastError() << endl;
         closesocket(connectSocket);
         WSACleanup();
         return 5;
     }
-    cout << "Send information" << endl;
+    cout << "send data" << endl;
+
     //5. HTTP Response
+
     string response;
+
     const size_t BUFFERSIZE = 1024;
     char resBuf[BUFFERSIZE];
+
     int respLength;
+
     do {
         respLength = recv(connectSocket, resBuf, BUFFERSIZE, 0);
         if (respLength > 0) {
-            response += string(resBuf).substr(0, respLength) + "\n";
+            response += string(resBuf).substr(0, respLength);
         }
         else {
-            cout << "Error accept -> " << WSAGetLastError() << endl;
+            cout << "recv failed: " << WSAGetLastError() << endl;
             closesocket(connectSocket);
             WSACleanup();
             return 6;
         }
+
     } while (respLength == BUFFERSIZE);
+
     cout << response << endl;
-    iResult = shutdown(connectSocket, SD_BOTH); 
+    string count;
+    cout << endl << endl;
+    cout << count << endl;
+    //отключает отправку и получение сообщений сокетом
+    iResult = shutdown(connectSocket, SD_BOTH);
     if (iResult == SOCKET_ERROR) {
-        cout << "Error disconnect -> " << WSAGetLastError() << endl;
+        cout << "shutdown failed: " << WSAGetLastError() << endl;
         closesocket(connectSocket);
         WSACleanup();
         return 7;
     }
+
     closesocket(connectSocket);
     WSACleanup();
 }
